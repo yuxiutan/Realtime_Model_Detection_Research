2405aace808fb8e80ddc917e61c1e84d19852097
TAN <donna60921@gmail.com>
2025-06-27 02:34:48 +0800

    Update wazuh_api.sh

--- utils/wazuh_api.sh
+++ utils/wazuh_api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -k -u admin:0000 -X GET "https://192.168.6.101:9200/wazuh-alerts-*/_search?pretty" -H 'Content-Type: application/json' -d'{
+curl -k -u admin:0000 -X GET "https://192.168.6.101:9200/wazuh-alerts-*/_search" -H 'Content-Type: application/json' -d'{
   "_source": ["agent.name", "agent.id", "agent.ip", "@timestamp", "rule.id", "rule.mitre.id", "full_log"],
   "query": {
     "bool": {
@@ -11,4 +11,12 @@ curl -k -u admin:0000 -X GET "https://192.168.6.101:9200/wazuh-alerts-*/_search?
   },
   "sort": [{"@timestamp": {"order": "desc"}}],
   "size": 100
+}' | jq -c '.hits.hits[] | ._source | {
+  "@timestamp": .["@timestamp"],
+  "agent.ip": .["agent.ip"],
+  "agent.name": .["agent.name"],
+  "agent.id": .["agent.id"],
+  "rule.id": .["rule.id"],
+  "rule.mitre.id": .["rule.mitre.id"],
+  "full_log": .["full_log"]
 }' >> /home/youruser/Realtime_Transformer_Chain_Detection/data/new_attack_data.jsonl
