691ff9644a21cc4b9b8502e59e420c59870dce8d
TAN <donna60921@gmail.com>
2025-08-25 17:19:13 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from .inference import predict
 from .scheduler import start_scheduler_thread
 
-app = FastAPI(title="Fully Automatic Transformer Detection API")
+app = FastAPI(title="Realtime Transformer Attack Chain API")
 
 @app.post("/predict")
 def run_inference(data: dict):
+    """
+    data = {"features": [f1, f2, f3, f4, f5, f6]}
+    """
     return {"result": predict([data["features"]])}
 
 # 啟動背景自動 retrain
